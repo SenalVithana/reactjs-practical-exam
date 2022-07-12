5349cbe15539a71b2aa84a22c93e74fa05a9f822
SenalVithana <senalpulindu@gmail.com>
2022-07-12 19:55:35 +0530

    Updates

---
+++ precache-manifest.844f52d5d86398d965a2d9acb54157e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8643c9a7d8e5123a510",
+    "url": "/reactjs-practical-exam/static/css/main.6aa5f7d7.chunk.css"
+  },
+  {
+    "revision": "b8643c9a7d8e5123a510",
+    "url": "/reactjs-practical-exam/static/js/main.da4192d8.chunk.js"
+  },
+  {
+    "revision": "4029b05c74513cd6db68",
+    "url": "/reactjs-practical-exam/static/js/runtime~main.f4fd5cc2.js"
+  },
+  {
+    "revision": "c40ecdd3ae44c496e4e4",
+    "url": "/reactjs-practical-exam/static/js/2.0c0ce735.chunk.js"
+  },
+  {
+    "revision": "dfef2813c51b7c1e772e",
+    "url": "/reactjs-practical-exam/static/js/3.c015642e.chunk.js"
+  },
+  {
+    "revision": "449324f6c8889137e5b4ed2410ca50b9",
+    "url": "/reactjs-practical-exam/static/media/default.449324f6.jpg"
+  },
+  {
+    "revision": "014db5d5757273e45d4fb73b041e33a9",
+    "url": "/reactjs-practical-exam/index.html"
+  }
+];
